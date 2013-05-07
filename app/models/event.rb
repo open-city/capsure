29f7990a5488e1363c0d66934393112258d51074
Derek Eder <derek.eder@gmail.com>
2013-05-07 17:06:04 -0500

    updated date format on district detail page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,10 @@ class Event < ActiveRecord::Base
   attr_accessible :start_date, :end_date, :name, :details, :url, :contact_details, :location, :modified_date
 
   def start_date_formatted
+    start_date.strftime("%b #{start_date.day.ordinalize}, %Y")
+  end
+
+  def start_date_formatted_full
     start_date.strftime("%A, %b #{start_date.day.ordinalize}, %Y")
   end
 
