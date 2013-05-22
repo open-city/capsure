42512f2375760ea822f4b54f90b39aaa9ec827d3
Christopher Manning <c@christophermanning.org>
2013-05-21 19:50:08 -0500

    Add link to caps event in the details field of a gcal event

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ CapsCalendar::Application.routes.draw do
   match '/district/:id(/:status)' => 'calendar#show'
 
   match '/district/:id/beat/:beat_id/next' => 'event#next_event'
-  match '/district/:id/event/:event_slug' => 'event#show'
+  match '/district/:id/event/:event_slug' => 'event#show', :as => :event
   match '/district/:id/event/:event_slug/ical' => 'event#ical'
 
   get '/browse' => 'calendar#index'
