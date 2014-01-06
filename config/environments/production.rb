5dda9112bf2541a7369bdca484b4a38ef9ac92a0
Derek Eder <derek.eder@gmail.com>
2014-01-06 12:47:01 -0600

    tweaked asset pipeline config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,10 @@ CapsCalendar::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
